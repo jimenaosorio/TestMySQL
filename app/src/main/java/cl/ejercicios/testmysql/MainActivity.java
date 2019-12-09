da87f994a68a4d8d97baa81f0bcb15a4fb93d3bf
jimenaosorio <jimena.b.osorio@gmail.com>
2019-12-09 20:05:15 -0300

    Initial commit

---
+++ app/src/main/java/cl/ejercicios/testmysql/MainActivity.java
@@ -0,0 +1,66 @@
+package cl.ejercicios.testmysql;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    EditText nombre;
+    EditText telefono;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nombre=(EditText)findViewById(R.id.nombre);
+        telefono=(EditText)findViewById(R.id.telefono);
+    }
+    public void ingresarClick(View view)
+    {
+        ejecutarServicio("http://10.34.18.254/webservices/insertar.php"); //Mirar el IP con ipconfig (Windows) o ifconfig (Linux-Mac)
+
+
+    }
+
+    private void ejecutarServicio(String url)
+    {
+        StringRequest stringRequest=new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(getApplicationContext(), "Conexion OK", Toast.LENGTH_SHORT).show();
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),error.toString(),Toast.LENGTH_SHORT).show();
+            }
+        }
+        ){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parametros=new HashMap<String, String>();
+                parametros.put("nombre", nombre.getText().toString());
+                parametros.put("telefono", telefono.getText().toString());
+                return parametros;
+            }
+        };
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+}
